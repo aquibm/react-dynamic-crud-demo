beb45c6e067ce378b03cc5c8f28ae7b34a6a2b31
Aquib Master <aquibmaster@gmail.com>
2017-10-31 21:07:19 +1300

    Load and render out schema for routes

--- client/src/pages/Entity/Add.js
+++ client/src/pages/Entity/Add.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PageHoc from 'hocs/PageHoc'
+import { loadSchema } from 'api/loadSchema'
 
-const AddEntityPage = ({ entityType }) => (
-    <div>Adding Entity of type {entityType}</div>
-)
+class AddEntityPage extends PureComponent {
+    state = {
+        schema: {},
+    }
+
+    componentDidMount() {
+        const { entityType } = this.props
+        loadSchema(entityType).then(schema => this.setState({ schema }))
+    }
+
+    render() {
+        const { schema } = this.state
+        return <div>{JSON.stringify(schema)}</div>
+    }
+}
 
 export default PageHoc(AddEntityPage)
