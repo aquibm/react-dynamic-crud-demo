fadbe4b5a97b49aa666b958615434390c2922c1e
Aquib Master <aquibmaster@gmail.com>
2017-10-31 21:06:52 +1300

    Enable cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const router = require('./router')
 
 const app = express()
@@ -7,6 +9,7 @@ const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api', router)
 
 app.listen(port, () => {
