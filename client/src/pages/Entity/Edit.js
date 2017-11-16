8717201af9fc4ce8daaca1acac91f332894fb697
Aquib Master <aquibmaster@gmail.com>
2017-11-16 16:20:46 +1300

    Build out the edit entity page

--- client/src/pages/Entity/Edit.js
+++ client/src/pages/Entity/Edit.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PageHoc from 'hocs/PageHoc'
+import { loadSchema, getEntity, updateEntity } from 'api/entity'
+import ModifyEntityForm from 'components/ModifyEntityForm'
 
-const EditEntityPage = ({ entityType, entityId }) => (
-    <div>
-        Editing Entity of type {entityType} with ID {entityId}
-    </div>
-)
+class EditEntityPage extends PureComponent {
+    state = {
+        schema: {},
+        entity: {},
+    }
+
+    componentDidMount() {
+        const { entityType, entityId } = this.props
+
+        const loadPromises = [loadSchema(entityType), getEntity(entityId)]
+
+        Promise.all(loadPromises).then(([schema, entity]) => {
+            this.setState({
+                schema,
+                entity,
+            })
+        })
+    }
+
+    _handleChange = fieldName => event => {
+        const { value } = event.target
+
+        this.setState(state => ({
+            schema: state.schema,
+            entity: {
+                ...state.entity,
+                fields: {
+                    ...state.entity.fields,
+                    [fieldName]: value,
+                },
+            },
+        }))
+    }
+
+    _persistChanges = () => {
+        const { entity } = this.state
+        const { history } = this.props
+
+        updateEntity(entity).then(() => {
+            history.push(`/entity/list/${entity.type}`)
+        })
+    }
+
+    render() {
+        const { schema, entity } = this.state
+        return (
+            <ModifyEntityForm
+                schema={schema}
+                entity={entity}
+                handleChange={this._handleChange}
+                persistChanges={this._persistChanges}
+            />
+        )
+    }
+}
 
 export default PageHoc(EditEntityPage)
